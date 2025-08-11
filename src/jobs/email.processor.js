d9ac4bc5c22fc08a2ff4699ca37746ca7e2940bd
viniciusabr <vinicius.abreu@gmail.com>
2025-08-11 09:51:06 -0300

    Ajuste na duplicidade

--- src/jobs/email.processor.js
+++ src/jobs/email.processor.js
@@ -54,51 +54,51 @@
 //nova versao sem duplicata de envio:
 
 
-import Customer from "../app/models/Customer.js";
-import EmailLog from "../app/models/EmailLog.js";
-import generateSurveyReminder from "../app/utils/generateSurveyReminder.js";
-import { sendSurveyReminderEmail } from "../app/utils/send.email.js";
-import logger from "../config/logger.js";
-import emailQueue from "../queues/email.queue.js";
+//import Customer from "../app/models/Customer.js";
+//import EmailLog from "../app/models/EmailLog.js";
+//import generateSurveyReminder from "../app/utils/generateSurveyReminder.js";
+//import { sendSurveyReminderEmail } from "../app/utils/send.email.js";
+//import logger from "../config/logger.js";
+//import emailQueue from "../queues/email.queue.js";
 
-emailQueue.process(1, async (job) => {
-  const { name, email, caso, user_id } = job.data;
+//emailQueue.process(1, async (job) => {
+ // const { name, email, caso, user_id } = job.data;
 
-  const tentativaAtual = job.attemptsMade + 1;
-  const maxTentativas = job.opts.attempts;
+//  const tentativaAtual = job.attemptsMade + 1;
+//  const maxTentativas = job.opts.attempts;
 
-  logger.info(`📤 [PROCESSOR] Tentativa ${tentativaAtual}/${maxTentativas} — Enviando e-mail para: ${email}`);
+//  logger.info(`📤 [PROCESSOR] Tentativa ${tentativaAtual}/${maxTentativas} — Enviando e-mail para: ${email}`);
 
   // Controle de status do envio
-  let envioStatus = 'SUCCESS';
-  let errorMessage = null;
+//  let envioStatus = 'SUCCESS';
+//  let errorMessage = null;
 
-  try {
-    const emailContent = generateSurveyReminder(name, caso);
-    await sendSurveyReminderEmail(email, emailContent);
-    logger.info(`✅ [PROCESSOR] E-mail enviado para: ${email}`);
-  } catch (err) {
-    envioStatus = 'FAILED';
-    errorMessage = err.message;
-    logger.error(`❌ [PROCESSOR] Erro ao enviar e-mail: ${email} | ${err.message}`);
-    throw err; // Bull reprocessa
-  } finally {
-    try {
-      await EmailLog.create({
-        user_id,
-        recipient_email: email,
-        caso,
-        envio_status: envioStatus,
-        sent_at: envioStatus === 'SUCCESS' ? new Date() : null,
-        error_message: errorMessage,
-      });
-      logger.info(`📥 [PROCESSOR] Log salvo: ${email} | Status: ${envioStatus}`);
-    } catch (err) {
-      logger.error(`❌ [PROCESSOR] Falha ao salvar log de ${envioStatus}: ${email} | ${err.message}`);
+//  try {
+//    const emailContent = generateSurveyReminder(name, caso);
+//    await sendSurveyReminderEmail(email, emailContent);
+//    logger.info(`✅ [PROCESSOR] E-mail enviado para: ${email}`);
+// } catch (err) {
+//    envioStatus = 'FAILED';
+  //  errorMessage = err.message;
+   // logger.error(`❌ [PROCESSOR] Erro ao enviar e-mail: ${email} | ${err.message}`);
+   // throw err; // Bull reprocessa
+  ///} finally {
+    //try {
+     // await EmailLog.create({
+    //    user_id,
+     //   recipient_email: email,
+  //      caso,
+  //      envio_status: envioStatus,
+  //      sent_at: envioStatus === 'SUCCESS' ? new Date() : null,
+  //      error_message: errorMessage,
+ //     });
+//      logger.info(`📥 [PROCESSOR] Log salvo: ${email} | Status: ${envioStatus}`);
+//    } catch (err) {
+//      logger.error(`❌ [PROCESSOR] Falha ao salvar log de ${envioStatus}: ${email} | ${err.message}`);
       // Não lança erro — log não impacta job
-    }
-  }
-});
+//    }
+// }
+//});
 // import Customer from "../app/models/Customer.js";
 // import EmailLog from "../app/models/EmailLog.js";
 // import generateSurveyReminder from "../app/utils/generateSurveyReminder.js";
@@ -179,7 +179,7 @@ emailQueue.process(1, async (job) => {
   try {
     const __filename = fileURLToPath(import.meta.url);
      const __dirname = dirname(__filename);
-   const templatePath = path.join(__dirname, '..', 'app', 'utils', 'email-template-novo.mjml');
+    const templatePath = path.resolve(process.cwd(), 'src', 'app', 'utils', 'email-template-novo.mjml');
     const emailContent = generateSurveyReminder(
     templatePath,
     { nome: name, caso } 
