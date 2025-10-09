db077db0e8ffbaa9bbc36a038ea6b21e4dfcaaec
Silas-Oliveira <silas.oliveira8@gmail.com>
2025-10-09 19:39:43 -0300

    add variavel de ambiente redis para produção

--- src/queues/email.queue.js
+++ src/queues/email.queue.js
@@ -2,11 +2,15 @@ import Queue from 'bull'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const emailQueue = new Queue('emailQueue', {
-  redis: {
+const redisConfig = process.env.UPSTASH_REDIS_TLS_URL
+  ? process.env.UPSTASH_REDIS_TLS_URL
+  : {
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT)
-  },
+  }
+
+const emailQueue = new Queue('emailQueue', {
+  redisConfig,
   limiter: {
     max: 1,
     duration: 3000
